168b308e1dd130aa6271c9f98e4967838c0c8a0e
jkongie <jkongie@gmail.com>
2011-09-13 17:16:22 +0800

    Added an option to pick a colour for the placeholder text

--- src/jquery.placeholder.js
+++ src/jquery.placeholder.js
@@ -1,29 +1,33 @@
 (function($){
-  
+
   $.fn.placeholder = function(options) {
-    
+
     var settings = $.extend({
-      attribute: 'placeholder'
+      attribute: 'placeholder',
+      color: null
     },options||{});
-    
+
     var placeholder_text = $(this).attr('data-' + settings.attribute);
     // Setup the placeholder
     if ($(this).val() === ''){
       $(this).val(placeholder_text);
       $(this).addClass('placeholder-input');
+      if(settings.color != null){ $(this).css('color', settings.color); }
     }
 
     $(this).focus(function(){
       if ($(this).val() === placeholder_text) {
         $(this).val('');
         $(this).removeClass('placeholder-input');
+        if(settings.color){ $(this).css('color', ''); }
       }
     });
-    
+
     $(this).blur(function(){
       if ($(this).val() === '' ) {
         $(this).val(placeholder_text);
-        $(this).addClass('placeholder-input');
+        $(this).addClass('placeholder-input')
+        if(settings.color != null){ $(this).css('color', settings.color); }
       }
     });
   }
