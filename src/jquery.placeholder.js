6c21d61f5f48bddc85667c299dcabae9acf43a60
jkongie <jkongie@gmail.com>
2011-09-30 16:54:58 +0800

    Attach handlers to every matched element

--- src/jquery.placeholder.js
+++ src/jquery.placeholder.js
@@ -4,37 +4,35 @@
 
     var settings = $.extend({
       attribute: 'placeholder',
-      color: null,
-      submitPlaceholder: false
+      color: null
     },options||{});
 
-    var placeholder_text = $(this).attr('data-' + settings.attribute);
-    // Setup the placeholder
-    if ($(this).val() === '' || $(this).val() === placeholder_text){
-      $(this).val(placeholder_text);
-      $(this).addClass('placeholder-input');
-      if(settings.color != null){ $(this).css('color', settings.color); }
-    }
+    return this.each(function(){
 
-    $(this).focus(function(){
-      if ($(this).val() === placeholder_text) {
-        $(this).val('');
-        $(this).removeClass('placeholder-input');
-        if(settings.color){ $(this).css('color', ''); }
-      }
-    });
+      var placeholder_text = $(this).attr('data-' + settings.attribute);
 
-    $(this).blur(function(){
-      if ($(this).val() === '' ) {
+      // Setup the placeholder
+      if ($(this).val() === '' || $(this).val() === placeholder_text){
         $(this).val(placeholder_text);
-        $(this).addClass('placeholder-input')
+        $(this).addClass('placeholder-input');
         if(settings.color != null){ $(this).css('color', settings.color); }
       }
+
+      $(this).focus(function(){
+        if ($(this).val() === placeholder_text) {
+          $(this).val('');
+          $(this).removeClass('placeholder-input');
+          if(settings.color){ $(this).css('color', ''); }
+        }
+      });
+
+      $(this).blur(function(){
+        if ($(this).val() === '' ) {
+          $(this).val(placeholder_text);
+          $(this).addClass('placeholder-input')
+          if(settings.color != null){ $(this).css('color', settings.color); }
+        }
+      });
     });
-
-    if(submitPlaceholder){
-
-    }
-
   }
 })(jQuery);
